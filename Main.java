965665b83926c0d9071cd27d7c53de5da6233c99
jasontan0649 <jasontankaiyuan@gmail.com>
2023-07-31 13:48:40 +0000

    Updated source code with solution

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Helloworld!");
-  }
+    public static int sumOfNaturalNumsON(int begin, int end) {
+        int sum = 0;
+        for (int i = begin; i <= end; i++)
+            sum += i;
+        return sum;
+    }
+    
+    public static int sumOfNaturalNumsO1(int begin, int end) {
+        //Sum of AP with simplified formula to prevent conversion loss
+        return (end * (end + 1) - begin * (begin - 1)) / 2;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        int answerON = sumOfNaturalNumsON(5, 100);
+        System.out.println("Answer for O(n) Algo: " + answerON);
+        
+        int answerO1 = sumOfNaturalNumsO1(5, 100);
+        System.out.println("Answer for O(1) Algo: " + answerO1);
+    }
 }
